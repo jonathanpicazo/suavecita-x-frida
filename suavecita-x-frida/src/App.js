2a4320c916da1081f84c188e9d8716ae3ddd384d
jonathanpicazo <43653582+jonathanpicazo@users.noreply.github.com>
2021-09-17 03:32:41 -0700

    just need to add text

---
+++ suavecita-x-frida/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="head"></div>
+      <div className="line">A LINE OF HAIR CARE AND STYLING PRODUCTS<br></br>TO INSPIRE THE INNER ARTIST IN EVERYONE.</div>
+      <div className="asset2"></div>
+      <div className="suav">suav</div>
+      <div className="asset3">asset3</div>
+      <div className="choose">choose</div>
+      <div className="heat">heat</div>
+      <div className="asset4"></div>
+      <div className="asset5"></div>
+      <div className="styling">styling</div>
+      <div className="complete">complete</div>
+      <div className="asset6">asset6</div>
+      <div className="asset7">asset7</div>
+      <div className="brush"></div>
+      <div className="foot">
+      </div>
+    </div>
+  );
+}
+
+export default App;
