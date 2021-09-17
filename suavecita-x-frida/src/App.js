4325fdace8531d8254d70a99de92cce22a0cc8c4
jonathanpicazo <43653582+jonathanpicazo@users.noreply.github.com>
2021-09-17 06:29:11 -0700

    fix mobile viewports and add fonts

--- suavecita-x-frida/src/App.js
+++ suavecita-x-frida/src/App.js
@@ -6,9 +6,22 @@ function App() {
   return (
     <div className="container">
       <div className="head"></div>
-      <div className="line">A LINE OF HAIR CARE AND STYLING PRODUCTS<br></br>TO INSPIRE THE INNER ARTIST IN EVERYONE.</div>
+      <div className="line"><p>A LINE OF HAIR CARE AND STYLING PRODUCTS<br></br>TO INSPIRE THE INNER ARTIST IN EVERYONE.</p></div>
       <div className="asset2"></div>
-      <div className="suav">suav</div>
+      <div className="suav">
+        <p>
+        <hr></hr>
+        SUAVECITA'S HIGH <br></br>
+        QUALITY PRODUCTS<br></br>
+        WITH BEAUTIFUL<br></br>
+        LIMITED EDITION<br></br>
+        PACKAGING THAT<br></br>
+        PAYS HOMAGE TO<br></br>
+        <span>FRIDA KAHLO'S</span>
+        <br></br>
+        UNIQUE ARTWORK
+        </p>
+      </div>
       <div className="asset3">asset3</div>
       <div className="choose">choose</div>
       <div className="heat">heat</div>
@@ -20,6 +33,11 @@ function App() {
       <div className="asset7">asset7</div>
       <div className="brush"></div>
       <div className="foot">
+        <p>
+        FREE US SHIPPING WHEN YOU SPEND $35<br></br>
+        FREE CANADA & MEXICO SHIPPING WHEN YOU SPEND $75 <br></br>
+        </p>
+        <div className="suavlink">SUAVECITA.COM</div>
       </div>
     </div>
   );
