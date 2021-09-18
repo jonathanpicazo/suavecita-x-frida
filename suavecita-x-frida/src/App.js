45f67de45ed9dbcd674f1104c2aabf454a00b99d
jonathanpicazo <43653582+jonathanpicazo@users.noreply.github.com>
2021-09-17 22:37:21 -0700

    todo: finish footer

--- suavecita-x-frida/src/App.js
+++ suavecita-x-frida/src/App.js
@@ -11,33 +11,92 @@ function App() {
       <div className="suav">
         <p>
         <hr></hr>
-        SUAVECITA'S HIGH <br></br>
+        <br></br>
+        <div>
+        SUAVECITA'S HIGH </div>
         QUALITY PRODUCTS<br></br>
         WITH BEAUTIFUL<br></br>
         LIMITED EDITION<br></br>
         PACKAGING THAT<br></br>
         PAYS HOMAGE TO<br></br>
-        <span>FRIDA KAHLO'S</span>
+        <div className="firstword">FRIDA</div> <div className="firstword">KAHLO'S</div>
         <br></br>
         UNIQUE ARTWORK
+        <br></br>
+        <br></br>
+        <hr></hr>
+        <br></br>
         </p>
       </div>
       <div className="asset3">asset3</div>
-      <div className="choose">choose</div>
-      <div className="heat">heat</div>
+      <div className="choose">
+      <p>
+        <div className="p1">
+        CHOOSE FROM INDIVIDUAL HAIR PRODUCTS</div>
+        OR PICK UP ONE OF OUR CURATED SETS.
+        <hr></hr>
+      <div className="p2">
+        All products in this collection have our exclusive Fruta De La Vida <br></br>
+        fragrance. Tropical guava and pineapple complement the floral <br></br>
+        hibiscus, jasmine and sweet coconut water. The base note of<br></br>
+        driftwood adds grounding to this bright fragrance.
+      </div>
+      </p>
+      </div>
+      <div className="heat">
+        <ul>
+          <li><div className="firstword">HEAT</div> <div className="firstword">PROTECTANT</div> <div className="firstword">SPRAY</div></li>
+          <li><div className="firstword">ARGAN</div> <div className="firstword">SILKENING</div> <div className="firstword">SERUM</div></li>
+          <li><div className="firstword">DRY</div> <div className="firstword">SHAMPOO</div> <div className="firstword">SPRAY</div></li>
+          <li><div className="firstword">HYDRATING</div> <div className="firstword">SHAMPOO</div></li>
+          <li><div className="firstword">HYDRATING</div> <div className="firstword">CONDITIONER</div></li>
+        </ul>
+      </div>
       <div className="asset4"></div>
       <div className="asset5"></div>
-      <div className="styling">styling</div>
-      <div className="complete">complete</div>
-      <div className="asset6">asset6</div>
-      <div className="asset7">asset7</div>
-      <div className="brush"></div>
+      <div className="styling">
+      <ul>
+          <li><div className="firstword">STYLING</div> <div className="firstword">SPRAY</div></li>
+          <li><div className="firstword">FIRM</div> <div className="firstword">HOLD</div> <div className="firstword">HAIRSPRAY</div></li>
+          <li><div className="firstword">SUAVECITA</div> <div className="firstword">POMADE</div></li>
+          <li><div className="firstword">SHINE</div> <div className="firstword">SPRAY</div></li>
+        </ul>
+      </div>
+      <div className="complete">
+        <p>
+          <div className="firstword">THE</div> <div className="firstword">COMPLETE</div><br></br>
+          <div className="firstword">COLLECTION</div> IS<br></br>
+          AVAILABLE IN A<br></br>
+          LIMITED EDITION,<br></br>
+          HAND NUMBERED<br></br>
+          BOX WITH TWO<br></br>
+          TIERS OF PRODUCTS<br></br>
+          AND A BEAUTIFUL<br></br>
+          POP-UP OF FRIDA<br></br>
+        </p>
+      </div>
+      <div className="asset6"></div>
+      <div className="asset7"></div>
+      <div className="brush">
+        <p>
+        <div className="title">BRUSH</div><div className="title">&#160;</div> <div className="title">SET</div>
+        <ol>
+          <li>- <div className="p3">PADDLE</div> <div className="p3">BRUSH</div></li>
+          <li> - <div className="p3"className="p3">SCULPTING</div> <div className="p3">BRUSH</div></li>
+          <li> - <div className="p3">TEASING <div className="p3">BRUSH</div></div></li>
+          <li> - <div className="p3">WET <div className="p3">BRUSH</div></div> -</li>
+        </ol>
+        <hr></hr>
+          <div className="engraved">These engraved bamboo brushed are only available as a set</div>
+          <div className="copyright">&#169;Frida Kahlo Corporation</div>
+          </p>
+      </div>
       <div className="foot">
         <p>
         FREE US SHIPPING WHEN YOU SPEND $35<br></br>
         FREE CANADA & MEXICO SHIPPING WHEN YOU SPEND $75 <br></br>
-        </p>
         <div className="suavlink">SUAVECITA.COM</div>
+        </p>
       </div>
     </div>
   );
